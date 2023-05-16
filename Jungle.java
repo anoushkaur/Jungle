bd619f730d52c77e9bcca59d872819973da8fa6c
dwtsluna <dwtsluna@gmail.com>
2023-05-17 00:14:33 +0800

    Added comments in Jungle

--- Jungle.java
+++ Jungle.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
 
-public class Jungle {
+public class Jungle { 
    public static void main(String[] args){
      // Animal a = new Rat(GameSystem.P2);
     //  System.out.print(a);
-      GameSystem gs = new GameSystem();
+      GameSystem gs = new GameSystem(); // Instantiating classes
       Displayer disp = new Displayer();
       Scanner input = new Scanner(System.in);
       //System.out.println(gs.GetBoard()[3][5] instanceof River);
       
       //System.out.println(gs.GetAnimalPos(gs.GetCurrPlayer().GetAnimals()[6])[0]);
       //System.out.println(gs.GetAnimalPos(gs.GetCurrPlayer().GetAnimals()[6])[1]);
-      while (true){
-         disp.PrintBoard(gs.GetBoard());
+      
+      while (true){ // Player's Menu
+         disp.PrintBoard(gs.GetBoard()); // Displaying board
          Animal[] animals = gs.GetCurrPlayer().GetAnimals();
-         disp.PrintValidAnimals(animals);
-         System.out.println("Choose animal");
+         disp.PrintValidAnimals(animals); // Displaying animals
+         System.out.println("Choose animal"); // Prompt user for which animal to move
          int animal = input.nextInt();
          boolean[] moves = gs.PossibleMoves(animals[animal-1]);
-         disp.PrintValidMoves(moves);
-         System.out.println("Choose dir");
+         disp.PrintValidMoves(moves); // Displaying possible moves
+         System.out.println("Choose dir"); // Prompt user for direction
          int dir = input.nextInt();
          int count = 0;
          int move = 0;
-         for (int i = 0; i<moves.length; i++){
-            if(moves[i]){
+         for (int i = 0; i < moves.length; i++){ 
+            if (moves[i]){
                count ++;
             }
             if (count == dir-1){
@@ -32,7 +33,7 @@ public class Jungle {
                break; //save i val
             }
          }
-         System.out.println(move);
+         System.out.println(move); 
          gs.Move(move, gs.GetCurrPlayer().GetAnimals()[animal-1]);
          move = 0;
       }
