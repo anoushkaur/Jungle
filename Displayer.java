02e0e1ae5af1695f0a186a2d0c368adce561087c
Anoushka Kaur <anoushkaur@gmail.com>
2023-05-18 13:07:31 +0800

    Removed dynamic animal number changing

--- Displayer.java
+++ Displayer.java
@@ -54,7 +54,7 @@ public class Displayer {
    
    public void PrintValidAnimals( Animal[] arr ){   
       for(int i = 0; i<arr.length; i++){
-         System.out.println(i+1 + ". " + arr[i]);         
+         System.out.println(arr[i].GetRank() + ". " + arr[i]);         
       }
    }
 }
