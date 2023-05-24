ece30f88a431cfc54381667dee479cb183111162
Anoushka Kaur <anoushkaur@gmail.com>
2023-05-24 13:08:56 +0800

    Added JUNGLE

--- Jungle.java
+++ Jungle.java
@@ -11,14 +11,23 @@ public class Jungle {
       int[] possibleMoves = new int[4];
       boolean animalFound = false;
       boolean GameOver = false;
-
+      System.out.printf("\n%53s" , "     __                     .__          ");
+      System.out.printf("\n%53s" ,"    |__|__ __  ____    ____ |  |   ____  ");
+      System.out.printf("\n%53s" ,"    |  |  |  \\/    \\  / ___\\|  | _/ __ \\ ");
+      System.out.printf("\n%53s" ,"    |  |  |  /   |  \\/ /_/  >  |_\\  ___/ ");
+      System.out.printf("\n%54s" ,"/\\__|  |____/|___|  /\\___  /|____/\\____/  ");
+      System.out.printf("\n%51s" ,"\\______|          \\//_____/            ");
+      System.out.println();
+      System.out.println();
+      
       while (!GameOver) { // Player's Menu
          animalFound = false;
          disp.PrintBoard(gs.GetBoard()); // Displaying board
          Animal[] animals = gs.GetCurrPlayer().GetAnimals();
-
+         
+         System.out.println("PLAYER " + gs.GetCurrPlayer().GetId() + " TURN:");
          disp.PrintValidAnimals(animals); // Displaying animals
-         System.out.println("Choose animal"); // Prompt user for which animalChoiceto move
+         System.out.println("Choose animal: "); // Prompt user for which animalChoiceto move
          int animalChoice = input.nextInt();
          
          while (!animalFound) { // while animalChoiceis not found in arr
@@ -34,7 +43,7 @@ public class Jungle {
 
          boolean[] moves = gs.PossibleMoves(animals[animalChoice - 1]);
          if (moves[0] == false && moves[1] == false && moves[2] == false && moves[3] == false){//if no moves avail
-            System.out.println("No Moves Available! Choose a different animalChoicebelow: ");
+            System.out.println("No Moves Available! Choose a different animal below: ");
          }
          else { //if at least 1 move avail
             disp.PrintValidMoves(moves); // Displaying possible moves
