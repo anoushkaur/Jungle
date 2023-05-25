0c08b160a40f8dc10547e4492551fa542d78bae7
dwtsluna <dwtsluna@gmail.com>
2023-05-26 00:59:06 +0800

    Edited some displaying words

--- Jungle.java
+++ Jungle.java
@@ -17,23 +17,26 @@ public class Jungle {
       System.out.printf("\n%53s" ,"    |  |  |  /   |  \\/ /_/  >  |_\\  ___/ ");
       System.out.printf("\n%54s" ,"/\\__|  |____/|___|  /\\___  /|____/\\____/  ");
       System.out.printf("\n%51s" ,"\\______|          \\//_____/            ");
+      System.out.printf("\n\n%53s" ,"Written by Sofea & Anoushka for ICS4U CCT");
       System.out.println();
       System.out.println();
       
       while (!GameOver) { // Player's Menu
          animalFound = false;
          disp.PrintBoard(gs.GetBoard()); // Displaying board
+         //Animal[] animals = gs.GetCurrPlayer().GetAnimals();
+         System.out.println("==================");
+         System.out.println("| PLAYER " + gs.GetCurrPlayer().GetId() + " TURN: |");
+         System.out.println("==================");
          Animal[] animals = gs.GetCurrPlayer().GetAnimals();
-         
-         System.out.println("PLAYER " + gs.GetCurrPlayer().GetId() + " TURN:");
          disp.PrintValidAnimals(animals); // Displaying animals
-         System.out.println("Choose animal: "); // Prompt user for which animalChoiceto move
+         System.out.print("Choose an animal: "); // Prompt user for which animalChoiceto move
          int animalChoice = input.nextInt();
          
          while (!animalFound) { // while animalChoiceis not found in arr
             if (animalChoice < 1 || animalChoice > 8 || animals[animalChoice-1] == null){ // error message for wrong animal
 
-                System.out.println("Enter a valid animal: ");
+                System.out.print("Error! Invalid animal. Please enter a valid animal: ");
                 animalChoice = input.nextInt();
             }
             else {                
@@ -47,7 +50,7 @@ public class Jungle {
          }
          else { //if at least 1 move avail
             disp.PrintValidMoves(moves); // Displaying possible moves
-            System.out.println("Choose dir: "); // Prompt user for direction
+            System.out.print("Choose a direction: "); // Prompt user for direction
             int dir = input.nextInt();
             //for loop checking how many moves are true in the possible moves arr
             int count = 0; 
@@ -59,7 +62,7 @@ public class Jungle {
                }
             }
             while (dir < 1 || dir > count){
-               System.out.println("Enter a valid direction: ");
+               System.out.print("Error! Invalid direction. Please enter a valid direction: ");
                dir = input.nextInt();
             }
             gs.Move(temp[dir-1], animals[animalChoice- 1]);
